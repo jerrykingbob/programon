54b5e95a6a41359d36845be5fe986db57f7ae594
LukeHuckman <LukeHuckman@users.noreply.github.com>
2018-11-26 19:35:07 +0800

    Added play class for main battle sequence

---
+++ src/programon/play.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programon;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+/**
+ *
+ * @author RotiCanai
+ */
+public class play extends JFrame{
+    public static void singleplayer(){
+        //Singleplayer logic goes here
+    }
+    
+    public static void multiplayer(){
+        //multiplayer logic goes here
+    }
+}
